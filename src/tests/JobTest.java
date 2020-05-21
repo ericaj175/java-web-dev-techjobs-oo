0593011829a816b9247753418f273bcae992a1de
Erica Jones <contacteljones@gmail.com>
2020-05-20 20:09:15 -0500

    Update job_data.csv
    Fixed Javascript typos in csv file.kk

--- src/tests/JobTest.java
+++ src/tests/JobTest.java
@@ -15,28 +15,29 @@ class JobTest {
     public void testSettingJobId() {
         Job1 = new Job ();
         Job2 = new Job ();
-        Job3 = new Job ();
+
 
         assertFalse ( this.Job1.getId () == this.Job2.getId () );
         assertTrue ( this.Job2.getId () - this.Job1.getId () == 1 );
     }
     @Test
     public void testJobConstructorSetsAllFields() {
-
-        new Job ("Product tester",
+        Job3 = new Job ();
+        new Job ( "Product tester",
                 new Employer ( "ACME" ),
                 new Location ( "Desert" ),
                 new PositionType ( "Quality control" ),
                 new CoreCompetency ( "Persistence" ) );
-       //objectName instanceof ClassName
-        //Test the equals Method
-        // assertEquals (  );
 
-        assertTrue (Job3 instanceof Job);
+        assertTrue ( Job3 instanceof Job );
+
     }
-};
+    @Test
+    public void testJobsForEquality()  {
+
+    }
 
 
-
+}
     // assertFalse ( condition:this.job1 == = this.job2)
      //assertFalse ( condiiton this.)
