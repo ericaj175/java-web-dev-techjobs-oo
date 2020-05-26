c28b7757535498a6f228a68e5aaf7fae138f5100
Erica Jones <contacteljones@gmail.com>
2020-05-26 17:23:45 -0500

    Update job_data.csv
    Fixed Javascript typos in csv file.kk

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -1,6 +1,6 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
+
 
 public class Location extends JobField {
 
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,6 +1,6 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
+
 
 public class PositionType extends JobField {
 
