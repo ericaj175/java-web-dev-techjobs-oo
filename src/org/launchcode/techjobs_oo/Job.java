1d27b02ce4089e8d20d1106e9d0d9edd7ea43e93
Erica Jones <contacteljones@gmail.com>
2020-05-28 23:32:00 -0500

    k

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -43,56 +43,40 @@ public class Job {
     public int getId() {
         return id;
     }
-
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Employer getEmployer() {
         return employer;
     }
-
     public void setEmployer(Employer employer) {
         this.employer = employer;
     }
-
     public Location getLocation() {
         return location;
     }
-
     public void setLocation(Location location) {
         this.location = location;
     }
-
     public PositionType getPositionType() {
         return positionType;
     }
-
     public void setPositionType(PositionType positionType) {
         this.positionType = positionType;
     }
-
     public CoreCompetency getCoreCompetency() {
         return coreCompetency;
     }
-
-    public void setCoreCompetency(CoreCompetency coreCompetency) {
-        this.coreCompetency = coreCompetency;
-    }
+    public void setCoreCompetency(CoreCompetency coreCompetency) { this.coreCompetency = coreCompetency; }
 
     @Override
     public int hashCode() {
         return Objects.hash ( id, name, employer, location, positionType, coreCompetency );
     }
-
-
     public String toString() {
-
         String testString = "ID: " + this.getId ();
         String testString2 = "Name: " + this.getName ();
         String testString3 = "Employer: " + this.getEmployer ();
@@ -100,53 +84,38 @@ public class Job {
         String testString5 = "Position Type: " + this.getPositionType ();
         String testString6 = "Core Competency: " + this.getCoreCompetency ();
         String dataNotAvailable = "Data not available";
-
         return testString + "\n" +
                 testString2 + "\n" +
                 testString3 + "\n" +
                 testString4 + "\n" +
                 testString5 + "\n" +
                 testString6;
-    }{
-        String nameString;
-        if(this.getName() == null){
-        nameString = "Name:"  + "Data not available";
-                                    }
-        else {
-            nameString = "Name: " + this.getName ();
-            }
-           //
-            String employerString;
 
+        String nameString;
+        if(this.getName() == null) {
+        nameString = "Name: "+ "Data not available"; }
+        else
+            nameString = "Name: " + this.getName ();
+           ////////////
+            String employerString;
             if(this.getEmployer () == null){
-                employerString = "Employer: "  + "Data not available";
-                                            }
-            else {
-                employerString = "Employer:  " + this.getEmployer ();
-                    }
-                //
+                employerString = "Employer: "+ "Data not available"; }
+            else { employerString = "Employer: "+ this.getEmployer (); }
+                /////////
                 String locationString;
                 if(this.getLocation() == null){
-                    locationString = "Location"  + "Data not available";
-                                             }
-                else {
-                    locationString = "Location " + this.getLocation ();
+                    locationString = "Location: "+ "Data not available"; }
+                else { locationString = "Location: "+ this.getLocation ();
                      }
-                //
+                ////////
                     String positionTypeString;
                     if(this.getPositionType () == null){
-                        positionTypeString = "Position Type"  + "Data not available";
-                                                        }
-                    else {
-                        positionTypeString = "Position Type " + this.getPositionType ();
+                        positionTypeString = "Position Type: "+ "Data not available"; }
+                    else { positionTypeString = "Position Type: "+ this.getPositionType ();
                          }
-                        //
-
+                    //////////
                         String CoreCompetencyString;
-                        if(this.getCoreCompetency () == null){
-                            CoreCompetencyString = "Core Competency"  + "Data not available";
-                        }
-                        else {
-                            CoreCompetencyString = "Core Competency" + this.getCoreCompetency ();
-
-                        }}}
+                        if(this.getCoreCompetency () == null) {
+                            CoreCompetencyString = "Core Competency: "+ "Data not available"; }
+                        else CoreCompetencyString = "Core Competency: " + this.getCoreCompetency ();
+    }}
